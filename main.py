808df6589838418985de60ad23bd7bdd44cc4d80
Abdelrahman Hassan <66575436+Abdelrahmanhassan1@users.noreply.github.com>
2023-03-04 00:36:45 +0200

    adding code

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.comboBox.currentIndexChanged.connect(self.handle_combo_box)
+
         self.show_image()
-        self.load_matrices()
+        self.load_matrices_from_images()
         self.ui.image_frame.mousePressEvent = self.get_pixel_intensity
         
-
     @QtCore.pyqtSlot()
     def show_image(self):
         self.image = cv2.imread('./images/Shepp_logan.png')
@@ -23,7 +24,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.image = QtGui.QImage(self.image.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888).rgbSwapped()
         self.ui.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.image))
         self.ui.image_frame.setScaledContents(True)
-        self.create_the_corresponding_matrices(height, width)
+        # self.create_the_corresponding_matrices(height, width)
     
     def create_the_corresponding_matrices(self, height, width):
         # Create a matrix with the same shape as the loaded image
@@ -60,14 +61,14 @@ class MainWindow(QtWidgets.QMainWindow):
         cv2.imwrite('./images/T1Matrix.jpg', self.T1Matrix)
         
         self.T2Matrix = cv2.flip(self.T2Matrix, 1)
-        self.T1Matrix = cv2.rotate(self.T1Matrix, cv2.ROTATE_90_COUNTERCLOCKWISE) # Rotate clockwise
+        self.T2Matrix = cv2.rotate(self.T2Matrix, cv2.ROTATE_90_COUNTERCLOCKWISE) # Rotate clockwise
         cv2.imwrite('./images/T2Matrix.jpg', self.T2Matrix)
 
         self.PDMatrix = cv2.flip(self.PDMatrix, 1)
-        self.PDMatrix = cv2.rotate(self.PDMatrix, cv2.ROTATE_90_CLOCKWISE) # Rotate clockwise
+        self.PDMatrix = cv2.rotate(self.PDMatrix, cv2.ROTATE_90_COUNTERCLOCKWISE) # Rotate clockwise
         cv2.imwrite('./images/PDMatrix.jpg', self.PDMatrix)
 
-    def load_matrices(self):
+    def load_matrices_from_images(self):
         self.T1Matrix = np.loadtxt('T1Matrix.txt')
         self.T2Matrix = np.loadtxt('T2Matrix.txt')
         self.PDMatrix = np.loadtxt('PDMatrix.txt')
@@ -80,8 +81,54 @@ class MainWindow(QtWidgets.QMainWindow):
         y_scaled = int(y * scaling_factor)
         color = QtGui.QColor(self.image.pixel(x_scaled, y_scaled))
         intensity = color.red()
+        # get the values from the matrices
+        t1 = self.T1Matrix[x_scaled, y_scaled]
+        t2 = self.T2Matrix[x_scaled, y_scaled]
+        pd = self.PDMatrix[x_scaled, y_scaled]
+
+        self.ui.label_4.setText(str(t1))
+        self.ui.label_5.setText(str(t2))
+        self.ui.label_6.setText(str(pd))
+        print(f"T1: {t1}")
+        print(f"T2: {t2}")
+        print(f"PD: {pd}")
         print(f"Intensity: {intensity}")
     
+    def handle_combo_box(self):
+        if self.ui.comboBox.currentIndex() == 0:
+            self.image = cv2.imread('./images/Shepp_logan.png')
+            height, width, channel = self.image.shape
+            self.heightttt = height
+            bytesPerLine = 3 * width
+            self.image = QtGui.QImage(self.image.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.ui.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.image))
+            self.ui.image_frame.setScaledContents(True)
+        elif self.ui.comboBox.currentIndex() == 1:
+            self.T1Matrix = cv2.imread('./images/T1Matrix.jpg')
+            height, width, channel = self.T1Matrix.shape
+            self.heightttt = height
+            bytesPerLine = 3 * width
+            self.T1Matrix = QtGui.QImage(self.T1Matrix.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.ui.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.T1Matrix))
+            self.ui.image_frame.setScaledContents(True)
+
+        elif self.ui.comboBox.currentIndex() == 2:
+            self.T2Matrix = cv2.imread('./images/T2Matrix.jpg')
+            height, width, channel = self.T2Matrix.shape
+            self.heightttt = height
+            bytesPerLine = 3 * width
+            self.T2Matrix = QtGui.QImage(self.T2Matrix.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.ui.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.T2Matrix))
+            self.ui.image_frame.setScaledContents(True)        
+        elif self.ui.comboBox.currentIndex() == 3:
+            self.PDMatrix = cv2.imread('./images/PDMatrix.jpg')
+            height, width, channel = self.PDMatrix.shape
+            self.heightttt = height
+            bytesPerLine = 3 * width
+            self.PDMatrix = QtGui.QImage(self.PDMatrix.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.ui.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.PDMatrix))
+            self.ui.image_frame.setScaledContents(True)
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
