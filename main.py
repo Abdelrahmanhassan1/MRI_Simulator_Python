018b148addc13ea588239407ba9e3122d11d91f6
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-03-07 13:43:50 +0200

    adding features to phantom

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.comboBox.currentIndexChanged.connect(self.handle_combo_box)
+        self.ui.comboBox.currentIndexChanged.connect(
+            self.handle_image_features_combo_box)
         self.plot_horizontal_lines()
         self.redPen = pg.mkPen(color=(255, 0, 0))  # RED
         self.greenPen = pg.mkPen(color=(0, 255, 0))  # Green
@@ -36,24 +37,13 @@ class MainWindow(QtWidgets.QMainWindow):
         ]
 
         # phantom image
-        self.show_image_on_label()
+        self.show_image_on_label(
+            './images/phantom_modified/480px-Shepp_logan.png')
         self.modify_the_image_intensities_distribution()
         self.ui.phantom_image_label.mousePressEvent = self.handle_mouse_press
-        # self.show_image_histogram()
-        # self.get_unique_image_intensities()
 
     @QtCore.pyqtSlot()
-    def show_image_on_label(self):
-        try:
-            self.shepp_logan_image = QImage(
-                './images/phantom_modified/480px-Shepp_logan.png')
-            pixmap = QPixmap.fromImage(self.shepp_logan_image)
-            self.ui.phantom_image_label.setPixmap(pixmap)
-            # self.get_unique_image_intensities()
-        except Exception as e:
-            print(e)
-
-    def show_image(self, image_path):
+    def show_image_on_label(self, image_path):
         img = QImage(image_path)
         pixmap = QPixmap.fromImage(img)
         self.ui.phantom_image_label.setPixmap(pixmap)
@@ -69,9 +59,11 @@ class MainWindow(QtWidgets.QMainWindow):
             if pixmap is not None:
                 pixel_color = pixmap.toImage().pixel(x, y)
                 intensity = QColor(pixel_color).getRgb()[0]
-                t1 = self.T1Matrix[x, y]
-                t2 = self.T2Matrix[x, y]
-                pd = self.PDMatrix[x, y]
+                index = np.where(
+                    self.most_frequent == intensity)[0][0]
+                t1 = self.t1Weight[index]
+                t2 = self.t2Weight[index]
+                pd = self.PDWeight[index]
                 self.ui.label_4.setText(str(t1))
                 self.ui.label_5.setText(str(t2))
                 self.ui.label_6.setText(str(pd))
@@ -83,105 +75,63 @@ class MainWindow(QtWidgets.QMainWindow):
 
         img = cv2.imread('./images/480px-Shepp_logan.png', 0)
 
-        height, width = img.shape
-
-        # Flatten the image array
         pixels = img.flatten()
 
-        # Count the occurrences of each pixel intensity
         count = Counter(pixels)
 
-        # Find the top five most frequent pixel intensities
-        most_frequent = heapq.nlargest(10, count, key=count.get)
+        self.most_frequent = heapq.nlargest(10, count, key=count.get)
 
-        # save the most_frequent intensities in a txt file
+        self.most_frequent = np.sort(self.most_frequent)
+
         np.savetxt('./txt_files/most_frequent.txt',
-                   np.sort(most_frequent), fmt='%d')
-
-        # Print the top five most frequent pixel intensities and their counts
-        for i in most_frequent:
+                   np.sort(self.most_frequent), fmt='%d')
+        for i in self.most_frequent:
             print(f"Pixel intensity {i}: {count[i]}")
 
-        # Overwrite the image array to have the nearest value of the top five most frequent pixel intensities
         for i in range(256):
-            if i not in most_frequent:
-                nearest = min(most_frequent, key=lambda x: abs(x-i))
+            if i not in self.most_frequent:
+                nearest = min(self.most_frequent, key=lambda x: abs(x-i))
                 pixels[pixels == i] = nearest
 
-        # Reshape the modified array to its original shape
         modified_img = pixels.reshape(img.shape)
 
-        # save the image locally
         cv2.imwrite(
             './images/phantom_modified/480px-Shepp_logan.png', modified_img)
 
-        self.create_the_corresponding_matrices(
-            height=height, width=width, most_frequent_intensities=most_frequent, phantom_image=modified_img)
+        self.t1WeightedImage = np.zeros_like(modified_img)
+        self.t2WeightedImage = np.zeros_like(modified_img)
+        self.PDWeightedImage = np.zeros_like(modified_img)
 
-        return modified_img
+        self.t1Weight = [25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
+        self.t2Weight = [250, 225, 200, 175, 150, 125, 100, 75, 50, 25]
+        self. PDWeight = [255, 255, 255, 255, 255, 255, 255, 255, 255, 255]
 
-    def create_the_corresponding_matrices(self, height, width, most_frequent_intensities, phantom_image):
-        # Create a matrix with the same shape as the loaded image
-        self.T1Matrix = np.zeros((height, width))
-        self.T2Matrix = np.zeros((height, width))
-        self.PDMatrix = np.zeros((height, width))
+        for i, intensity in enumerate(self.most_frequent):
+            self.t1WeightedImage[modified_img == intensity] = self.t1Weight[i]
+            self.t2WeightedImage[modified_img == intensity] = self.t2Weight[i]
+            self.PDWeightedImage[modified_img == intensity] = self.PDWeight[i]
 
-        # Assign specific values at each intensity in the matrix
-        t1matrix = np.linspace(
-            start=0, stop=255, num=len(most_frequent_intensities))
-        t2matrix = np.linspace(
-            start=255, stop=0, num=len(most_frequent_intensities))
-        pdmatrix = np.repeat(255, len(most_frequent_intensities))
+        # Save the modified image and the three unique intensity images locally
+        cv2.imwrite('./images/features_images/t1WeightedImage.png',
+                    self.t1WeightedImage)
+        cv2.imwrite('./images/features_images/t2WeightedImage.png',
+                    self.t2WeightedImage)
+        cv2.imwrite('./images/features_images/PDWeightedImage.png',
+                    self.PDWeightedImage)
 
-        for intensity, t1, t2, pd in zip(most_frequent_intensities, t1matrix, t2matrix, pdmatrix):
-            for y in range(height):
-                for x in range(width):
-                    pixel_intensity = phantom_image[x, y]
-                    if pixel_intensity == intensity:
-                        self.T1Matrix[x, y] = t1
-                        self.T2Matrix[x, y] = t2
-                        self.PDMatrix[x, y] = pd
-
-        self.create_the_corresponding_images()
-        # write each matrix in a txt file
-        np.savetxt('T1Matrix.txt', self.T1Matrix, fmt='%d')
-        np.savetxt('T2Matrix.txt', self.T2Matrix, fmt='%d')
-        np.savetxt('PDMatrix.txt', self.PDMatrix, fmt='%d')
-
-        self.load_matrices_from_images()
-
-    def create_the_corresponding_images(self):
-        # The issue you are experiencing might be related to the difference in how OpenCV and Qt handle image orientation. OpenCV uses the BGR color format by default, while Qt uses the RGB format. This can cause the image to appear mirrored.
-
-        # self.T1Matrix = cv2.flip(self.T1Matrix, 1)
-        # self.T1Matrix = cv2.rotate(
-        #     self.T1Matrix, cv2.ROTATE_90_COUNTERCLOCKWISE)  # Rotate clockwise
-        cv2.imwrite('./images/features_images/T1Matrix.jpg', self.T1Matrix)
-
-        # self.T2Matrix = cv2.flip(self.T2Matrix, 1)
-        # self.T2Matrix = cv2.rotate(
-        #     self.T2Matrix, cv2.ROTATE_90_COUNTERCLOCKWISE)  # Rotate clockwise
-        cv2.imwrite('./images/features_images/T2Matrix.jpg', self.T2Matrix)
-
-        # self.PDMatrix = cv2.flip(self.PDMatrix, 1)
-        # self.PDMatrix = cv2.rotate(
-        #     self.PDMatrix, cv2.ROTATE_90_COUNTERCLOCKWISE)  # Rotate clockwise
-        cv2.imwrite('./images/features_images/PDMatrix.jpg', self.PDMatrix)
-
-    def load_matrices_from_images(self):
-        self.T1Matrix = cv2.imread('./images/features_images/T1Matrix.jpg', 0)
-        self.T2Matrix = cv2.imread('./images/features_images/T2Matrix.jpg', 0)
-        self.PDMatrix = cv2.imread('./images/features_images/PDMatrix.jpg', 0)
-
-    def handle_combo_box(self):
+    def handle_image_features_combo_box(self):
         if self.ui.comboBox.currentIndex() == 0:
-            self.show_image('./images/phantom_modified/480px-Shepp_logan.png')
+            self.show_image_on_label(
+                './images/phantom_modified/480px-Shepp_logan.png')
         elif self.ui.comboBox.currentIndex() == 1:
-            self.show_image('./images/features_images/T1Matrix.jpg')
+            self.show_image_on_label(
+                './images/features_images/t1WeightedImage.png')
         elif self.ui.comboBox.currentIndex() == 2:
-            self.show_image('./images/features_images/T2Matrix.jpg')
+            self.show_image_on_label(
+                './images/features_images/t2WeightedImage.png')
         elif self.ui.comboBox.currentIndex() == 3:
-            self.show_image('./images/features_images/PDMatrix.jpg')
+            self.show_image_on_label(
+                './images/features_images/PDWeightedImage.png')
 
     # MRI Sequence
     def plot_horizontal_lines(self):
