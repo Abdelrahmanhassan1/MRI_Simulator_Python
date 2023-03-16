80b30e6955e516f6377412c24068f835bce6143e
Etcho <hesham.shehatah00@eng-st.cu.edu.eg>
2023-03-16 14:39:45 +0200

    finallllllly suquence Done

--- FFT_IFFT_Kspace_for_image/MRI_sequence.py
+++ FFT_IFFT_Kspace_for_image/MRI_sequence.py
@@ -3,17 +3,17 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import math
-image = cv2.imread('test.png',0)
+# image = np.array([[1,2,3],[4,5,6],[7,8,9]])
+image = cv2.imread('test.png')
+image  =cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
 print(f"image = {image}")
 ROWS, COLS = image.shape
 print(f"ROWS of image = {ROWS} , COLS of image = {COLS}")
-
 ####create vector for each pixel
 image_to_matrix = np.zeros((ROWS, COLS, 3))
 for row in range (ROWS):
     for col in range(COLS):
         image_to_matrix[row,col] = [0, 0, image[row, col]]
-
 print(f"image_to_matrix = {image_to_matrix}")
 
 ###create k space
@@ -31,72 +31,64 @@ for row in range(ROWS):
 
 print(f"image in XY plane = {image_to_matrix}")
 
+X_Gradient_step = 360 /COLS
+Y_Gradient_step = 360 /ROWS
+print(f"X_Gradient_step = {X_Gradient_step} , Y_Gradient_step = {Y_Gradient_step}")
+##Apply Gradient for Y
+for Y_rows_total_gradient in range(ROWS):
 
-
-Gy_phases = np.arange ( (2*np.pi)/ROWS ,(2*np.pi) + (2*np.pi)/ROWS, (2*np.pi)/ROWS)
-Gx_phases = np.arange ( (2*np.pi)/COLS ,(2*np.pi) + (2*np.pi)/COLS , (2*np.pi)/COLS)
-##divide phses for x , y
-# print(f"Gy_phases = {Gy_phases}")
-# print(f"Gx_phases = {Gx_phases}")
-row_Kspace = -1
-col_Kspace = -1
-##apply Yphases & Xphases
-for  phase_y in  Gy_phases:
-    row_Kspace = row_Kspace + 1
-    matrix_in_xy = image_to_matrix.copy()
-    # print(f"phase_y = {phase_y}")
-    for row ,phase_y_C_divided in zip(range(ROWS ) , np.arange ( phase_y/ROWS ,phase_y +phase_y/ROWS, phase_y/ROWS) ) :
-        Ry = np.array([[math.cos(math.radians(phase_y_C_divided)), -math.sin(math.radians(phase_y_C_divided)), 0],
-                       [math.sin(math.radians(phase_y_C_divided)), math.cos(math.radians(phase_y_C_divided)), 0],
+    matrix_after_Y_G = image_to_matrix.copy() ##take copy as every time use Y gradient apply every thing for to matrix after move to x-y plane
+    print(f"gradient value before multi to each Row = { Y_rows_total_gradient * Y_Gradient_step}")
+    for row in range(ROWS):
+        angle = Y_rows_total_gradient * Y_Gradient_step * row
+        print(f"gradient value iterate = {row}  after multi to each Row = {angle}")
+        Ry = np.array([[math.cos(math.radians(angle)), -math.sin(math.radians(angle)), 0],
+                       [math.sin(math.radians(angle)), math.cos(math.radians(angle)), 0],
                        [0, 0, 1]])
-        # print(f"Ry = {Ry}")
-        # print(f"phase_y_C_divided = {phase_y_C_divided}")
         for col in range(COLS):
-            # print(f"row = {row} , col = {col}")
-            matrix_in_xy[row,col] = np.dot(Ry, matrix_in_xy[row, col])
-    col_for_x = -1
-    for phase_x in Gx_phases:
-        # print(f"phase_x = {phase_x}")
-        Rx = np.array([[math.cos(math.radians(phase_x)), -math.sin(math.radians(phase_x)), 0],
-                       [math.sin(math.radians(phase_x)), math.cos(math.radians(phase_x)), 0],
-                       [0, 0, 1]])
-        col_for_x = col_for_x + 1
-        for row in range(ROWS):
-            # print(f" row = {row} , col_for_x = {col_for_x}")
-            matrix_in_xy[row, col_for_x] = np.dot(Rx, matrix_in_xy[row, col_for_x])
-        col_Kspace = col_Kspace + 1
-        if (col_Kspace == COLS):
-            col_Kspace = 0
+            print(f"row = {row} , col = {col}")
+            matrix_after_Y_G[row, col] = np.dot(Ry, matrix_after_Y_G[row, col])
+            print(f"matrix_in_xy[row,col] = {matrix_after_Y_G[row, col]}")
 
+
+
+    ##Apply Gradient for X
+    for X_cols_total_gradient in range(COLS):
+        matrix_after_Y_G_X_G = matrix_after_Y_G.copy()
+        print(f"gradient value before multi to each Column = {X_cols_total_gradient * X_Gradient_step}")
+        for col in range(COLS):
+            angle = X_cols_total_gradient * X_Gradient_step * col
+            print(f"gradient value iterate = {col}  after multi to each Row = {angle}")
+            Rx = np.array([[math.cos(math.radians(angle)), -math.sin(math.radians(angle)), 0],
+                           [math.sin(math.radians(angle)), math.cos(math.radians(angle)), 0],
+                           [0, 0, 1]])
+            for row in range(ROWS):
+                print(f"row = {row} , col = {col}")
+                matrix_after_Y_G_X_G[row, col] = np.dot(Rx, matrix_after_Y_G_X_G[row, col])
+                print(f"matrix_in_xy[row,col] = {matrix_after_Y_G_X_G[row, col]}")
+
+        x_sum = 0
+        y_sum = 0
         for i in range(ROWS):
             for j in range(COLS):
+                x_sum += matrix_after_Y_G_X_G[i,j,0]
+                y_sum += matrix_after_Y_G_X_G[i,j,1]
+        k_space[Y_rows_total_gradient, X_cols_total_gradient] = complex(y_sum, x_sum)  # complex k_space np.sqrt(x_sum**2 + y_sum**2)
+        print(f" matrix_in_xy after k space =  {matrix_after_Y_G_X_G}")
 
-                # print(f"row_Kspace = {row_Kspace} , col_Kspace = {col_Kspace}")
-                k_space[row_Kspace, col_Kspace] += np.sqrt(matrix_in_xy[i,j,0] ** 2 + matrix_in_xy[i,j,1] ** 2) * np.exp(complex(0, -(phase_x * j + phase_y * i)))
-        # print(f"k_space = {k_space}")
-
+        print(f"k_space = {k_space}")
 
 img_c3 = np.fft.ifftshift(k_space)
 img_c5 = np.fft.fftshift(k_space)
-img_c4 = np.fft.ifft2(img_c3)
-# print(f"img_c4 = {img_c4}")
-for row in range(ROWS):
-    for col in range (COLS):
-        img_c4[row, col] = np.sqrt(img_c4[row, col].real ** 2 + img_c4[row, col].imag ** 2)
-# print(f"img_c4 = {img_c4}")
-# print(f"matrix_in_xy[1, 0] = {matrix_in_xy[1, 0]}")
-# print(f"matrix_in_xy[i, j, 0] = {matrix_in_xy[1, 0, 0]} , matrix_in_xy[i, j, 1] = {matrix_in_xy[1, 0, 1]}")
+img_c4 = np.fft.ifft2(k_space)
 
 
-# print(f"RF to make M0 in plane XY = {R} ")
-# print(f"np.cos(80) = {np.cos(80)}")
-# print(f"np.cos(np.radians(80)) = {np.cos(np.radians(80))}")
-# print(f"np.cos(np.pi*2/9) = {np.cos((np.pi*2)/9)}")
-# print(f"np.cos(np.radians(np.pi/2)) = {np.cos(np.radians((3.141592653589793*2)/9))}")
-# print(f"Pi = {np.pi}")
-plt.subplot(141), plt.imshow(image, "gray"), plt.title("image")
-plt.subplot(142), plt.imshow(np.log(1+np.abs(k_space)), "gray"), plt.title("K space (Decentralized)")
-plt.subplot(143), plt.imshow(np.log(1+np.abs(abs(img_c3))), "gray"), plt.title("centralized")
-plt.subplot(144), plt.imshow(abs(img_c4), "gray"), plt.title("reconstructed image")
+plt.subplot(151), plt.imshow(image, "gray"), plt.title("image")
+plt.subplot(152), plt.imshow(np.log(1+np.abs(k_space)), "gray"), plt.title("K space ")
+plt.subplot(153), plt.imshow(np.log(1+np.abs(abs(img_c5))), "gray"), plt.title(" shift")
+plt.subplot(154), plt.imshow(np.log(1+np.abs(abs(img_c3))), "gray"), plt.title("inverse shift")
+plt.subplot(155), plt.imshow(abs(img_c4), "gray"), plt.title("reconstructed image")
 #
 plt.show()
+
+
